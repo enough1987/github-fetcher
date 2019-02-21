7c9f8adb47d67f336700719bce52573691185db8
Oleksandr Tilha <Oleksandr_Tilha@epam.com>
2019-02-21 14:03:46 +0800

    add new component - guess list

---
+++ src/GuessList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class GuessList extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    const guesses = this.props.guesses.length ? this.props.guesses.map((item, index) => {
+        return <div data-test="component-guess-item" key={index}> { item.guess } : { item.mutch } </div>
+    }) : "There are no guesses yet";
+
+    return (
+      <div className="component-guess-list"
+        data-test="component-guess-list" >
+          { guesses }
+      </div>
+    );
+  }
+}
+
+GuessList.propTypes = {
+    guesses: PropTypes.arrayOf(
+        PropTypes.shape({
+            guess: PropTypes.string,
+            mutch: PropTypes.number
+          }
+    ))
+};
+
+export default GuessList;
