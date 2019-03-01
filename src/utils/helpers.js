66afd454b026a082712460b43661b3ce958cc75a
otilha <otilha@corelogic.com>
2019-03-01 20:30:58 +0800

    lint

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,8 +2,4 @@ export const getMatchCount = (text, matcher) => {
   const textSet = new Set(text.split(''))
   const matcherSet = new Set(matcher.split(''))
   return [ ...textSet ].filter(lettter => matcherSet.has(lettter)).length
-
-  if ( true) {
-    
-  }
 }
