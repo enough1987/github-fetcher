4c4058f05e103ffbadb661d911636d8c6a91d5de
otilha <otilha@corelogic.com>
2019-03-04 20:35:40 +0800

    trigger ci

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,8 +1,8 @@
 export const matchWords = (a, b) => {
   const left = new Set(a);
   const right = new Set(b);
-
   const union = new Set();
+
   right.forEach((r) => {
     left.forEach((l) => {
       if (l === r) {
